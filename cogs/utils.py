a08bbe06674855f9c503ade960fc7d223427aa26
Frank Weaver <67629199+Zukashi2077@users.noreply.github.com>
2021-04-28 12:20:51 +0530

    Added suggest command

--- cogs/utils.py
+++ cogs/utils.py
@@ -5,7 +5,9 @@ import re
 import lxml.html as htm
 from urllib.parse import quote as urlfix
 from googletrans import Translator
-from langcodes import Language
+from langcodes import *
+import discord
+import datetime
 
 translator = Translator()
 
@@ -13,33 +15,11 @@ def cleanBraces(rtext):
   return re.sub(r"\[[^[]]*\]", "", rtext) # HAHAHAHAH GOT IT, GET REKT
 
 class Utils(commands.Cog, name = "Utilities"):
-"""Useful utilities. The best of EchoBoii!!"""
+    """Utility commands, most of them are from legacy version but rewritten for better output."""
     def __init__(self, bot):
-      self.bot = bot
-      self.weather_key = os.environ['OPENWEATHER_API_KEY']
-    
-    @commands.command(name="weather", aliases = ['weather'], brief = "Get current weather of city", description = "This command gets weather of the city passed on it. May or may not be accurate.")
-    async def weather(self, ctx,  *, city):
-        data = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={self.weather_key}").json()
-        
-        sorted_data = {
-            'City': data['name'],
-            'Weather': f"{data['weather'][0]['main']} - {data['weather'][0]['description']}",
-            'Temperature': f"{data['main']['temp']}°C",
-            'Humidity': f"{data['main']['humidity']}%",
-            'Pressure': f"{data['main']['pressure']}Pa",
-            'Clouds': f"{data['clouds']['all']}%",
-            'Wind': f"{data['wind']['speed']} km/h"
-        }
-        
-        embed = Embed(title=f":white_sun_small_cloud: Weather in {sorted_data['City']}", color=0x3498db)
-                      
-        for key, value in sorted_data.items():
-            embed.add_field(name=key, value=value)
-                      
-        await ctx.send(embed=embed)
+        self.bot = bot
 
-    @commands.command(name="wikipedia", aliases=['wiki', 'wikipedia'], brief="Get summary of a wikipedia article", description="This commands gets summary of a topic from a wikipedia article. Usage: eb wiki Bruno Mars")
+    @commands.command(name="Wikipedia", aliases=['wiki', 'wikipedia'], brief="Get summary of a wikipedia article", description="This commands gets summary of a topic from a wikipedia article. Usage: eb wiki Bruno Mars")
     async def wikipedia(self, ctx, *, query):
         data = requests.get('https://en.wikipedia.org/wiki/{}'.format(urlfix(query)))
         if data.status_code != 200:
@@ -47,11 +27,22 @@ class Utils(commands.Cog, name = "Utilities"):
         else:
             await ctx.send(cleanBraces(htm.fromstring(data.text.split('<p>')[1].split('</p>')[0]).text_content()))
 
-    @commands.command(name="translate", aliases=['gtrans', 'translate'], brief='Translate text to a wide variety of languages.', description='This command translates text to another specified language. Please use language codes like \'en\'/\'fr\'')
+    @commands.command(name="Google Translate", aliases=['gtrans', 'translate'], brief='Translate text to a wide variety of languages.', description='This command translates text to another language. Usage: eb translate en jus de chocolat (Output -> Chocolate Juice)')
     async def gtrans(self, ctx, langcode, *, text):
         tobj = translator.translate(text, dest=langcode)
         srcl = Language.make(language=tobj.src).display_name()
         await ctx.send('**__Original Text:__** {}\n**__Translated Text:__** {}\n**__Pronounciation:__** {}\n**__Source Language:__** {}'.format(text, tobj.text, tobj.pronunciation, srcl))
+
+    @commands.command(name='suggest', brief='Suggest something for the bot', description='This command sends us (the developers) a message. So you can give us a suggestion. Example: a suggestion to bring back a command from the legacy version of the bot.')
+    async def suggest(self, ctx, *, msg):
+        url = os.environ['WEBHOOK_URL']
+        timestamp = str(datetime.datetime.utcnow())
+        msg = 'DPY-001_{} (SUGESSTION MESSAGE)\n```{}```'.format(timestamp, msg)
+        data = {
+            'content': msg
+        }
+
+        requests.post(url, data)
    
 def setup(bot):
     bot.add_cog(Utils(bot))
