6b5bc5aef8a970d12c10afca5f19be128ee3310a
Frank Weaver <67629199+Zukashi2077@users.noreply.github.com>
2021-04-28 10:43:15 +0530

    Added proper descriptions

--- cogs/utils.py
+++ cogs/utils.py
@@ -13,11 +13,12 @@ def cleanBraces(rtext):
   return re.sub(r"\[[^[]]*\]", "", rtext) # HAHAHAHAH GOT IT, GET REKT
 
 class Utils(commands.Cog, name = "Utilities"):
+"""Useful utilities. The best of EchoBoii!!"""
     def __init__(self, bot):
       self.bot = bot
       self.weather_key = os.environ['OPENWEATHER_API_KEY']
     
-    @commands.command(name="Weather", aliases = ['weather'], brief = "Get current weather of city", description = "This command gets weather of the city passed on it. May or may not be accurate.")
+    @commands.command(name="weather", aliases = ['weather'], brief = "Get current weather of city", description = "This command gets weather of the city passed on it. May or may not be accurate.")
     async def weather(self, ctx,  *, city):
         data = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={self.weather_key}").json()
         
@@ -38,7 +39,7 @@ class Utils(commands.Cog, name = "Utilities"):
                       
         await ctx.send(embed=embed)
 
-    @commands.command(name="Wikipedia", aliases=['wiki', 'wikipedia'], brief="Get summary of a wikipedia article", description="This commands gets summary of a topic from a wikipedia article. Usage: eb wiki Bruno Mars")
+    @commands.command(name="wikipedia", aliases=['wiki', 'wikipedia'], brief="Get summary of a wikipedia article", description="This commands gets summary of a topic from a wikipedia article. Usage: eb wiki Bruno Mars")
     async def wikipedia(self, ctx, *, query):
         data = requests.get('https://en.wikipedia.org/wiki/{}'.format(urlfix(query)))
         if data.status_code != 200:
@@ -46,7 +47,7 @@ class Utils(commands.Cog, name = "Utilities"):
         else:
             await ctx.send(cleanBraces(htm.fromstring(data.text.split('<p>')[1].split('</p>')[0]).text_content()))
 
-    @commands.command(name="Google Translate", aliases=['gtrans', 'translate'], brief='Translate text to a wide variety of languages.', description='This command translates text to another language. Usage: eb translate en jus de chocolat (Output -> Chocolate Juice)')
+    @commands.command(name="translate", aliases=['gtrans', 'translate'], brief='Translate text to a wide variety of languages.', description='This command translates text to another specified language. Please use language codes like \'en\'/\'fr\'')
     async def gtrans(self, ctx, langcode, *, text):
         tobj = translator.translate(text, dest=langcode)
         srcl = Language.make(language=tobj.src).display_name()
