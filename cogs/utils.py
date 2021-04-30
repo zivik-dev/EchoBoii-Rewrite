66ff7a2763989724185e292592b41d58aecbbe03
Frank Weaver <67629199+Zukashi2077@users.noreply.github.com>
2021-04-30 21:58:41 +0530

    Added timein command

--- cogs/utils.py
+++ cogs/utils.py
@@ -7,7 +7,22 @@ from urllib.parse import quote as urlfix
 from googletrans import Translator
 from langcodes import *
 import discord
-import datetime
+import datetime, pytz
+from pytz.exceptions import UnknownTimeZoneError as UTZE
+import time
+
+def convert24(str1):
+    if str1[-2:] == "AM" and str1[:2] == "12":
+        return "00" + str1[2:-2]
+  
+    elif str1[-2:] == "AM":
+        return str1[:-2]
+
+    elif str1[-2:] == "PM" and str1[:2] == "12":
+        return str1[:-2]
+          
+    else:
+        return str(int(str1[:2]) + 12) + str1[2:8]
 
 translator = Translator()
 
@@ -45,6 +60,61 @@ class Utils(commands.Cog, name = "Utilities"):
         requests.post(url, data)
         
         await ctx.send('Message Sent :white_check_mark:')
+
+    @commands.command(name='timein', brief='Get your time in some other timezone!!', description='This command will convert a given time of your timezone to another specified timezone. Supports both 12-hour time and 24-hour time input. Outputs 24-hour time. Only accepts timezone regions like \'Asia/Kolkata\' as parameters. Formats like \'EST\' or \'IST\' are not allowed!')
+    async def timein(self, ctx, your_tz, convert_time, convertTo_tz):
+        local_time = convert_time
+        local_tz = your_tz
+        convert_tz = convertTo_tz
+
+        await ctx.send('Converting . . . /')
+
+        ltstemp = local_time.split(':')
+        ltstemp = [ltstemp[0], ltstemp[1].replace('AM', '').replace('PM', '')]
+        t1s, t2s = local_time.split(':')[0], local_time.split(':')[1]
+        if len(ltstemp[0]) != 2:
+            t1s = '0' + ltstemp[0]
+        if len(ltstemp[1]) != 2:
+            t2s = '0' + ltstemp[1]
+        newstr = t1s + ':' + t2s
+
+        if 'PM' in local_time.upper():
+            newstr += ' PM'
+        if 'AM' in local_time.upper():
+            newstr += 'AM'
+
+        local_time = newstr
+
+        del ltstemp, newstr, t1s, t2s
+
+        if 'PM' in local_time.upper():
+            meridien = local_time.split(' ')[1].strip()
+            time = local_time.split(' ')[0].split(':')
+            local_time = convert24(f'{time[0]}:{time[1]}:00 {meridien}')
+
+        if 'AM' in local_time.upper():
+            meridien = local_time.split(' ')[1].strip()
+            time = local_time.split(' ')[0].split(':')
+            local_time = convert24(f'{time[0]}:{time[1]}:00 {meridien}')
+
+        local_time = local_time.split(':')
+
+        try:
+            local_pytz = pytz.timezone(local_tz)
+        except UTZE:
+            await ctx.send('Timezones should be in the following format:\n`Asia/Kolkata`\nPlease avoid using format like:\n`IST` or `CEST`')
+
+        utcnow = datetime.datetime.utcnow()
+        timestr = f'{utcnow.year}-{utcnow.month}-{utcnow.day} {local_time[0]}:{local_time[1]}:{utcnow.second}.{utcnow.microsecond}'
+        datetime_tobj = datetime.datetime.strptime(timestr, '%Y-%m-%d %H:%M:%S.%f')
+
+        try:
+            converted_dtobj = datetime_tobj.astimezone(pytz.timezone(convert_tz))
+        except UTZE:
+            await ctx.send('Timezones should be in the following format:\n`Asia/Kolkata`\nPlease avoid using format like:\n`IST` or `CEST`')
+
+        await ctx.send('**Converted Time:** {}:{}'.format(converted_dtobj.hour, converted_dtobj.minute))
+
    
 def setup(bot):
     bot.add_cog(Utils(bot))
