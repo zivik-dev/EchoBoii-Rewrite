c33aa4c28a5735cf782a6bbd22836a0d4bbc4449
Frank Weaver <67629199+Zukashi2077@users.noreply.github.com>
2021-04-30 17:51:12 +0530

    Added no action for CommandNotFound

--- cogs/errorHandler.py
+++ cogs/errorHandler.py
@@ -6,29 +6,36 @@ import datetime
 import os
 
 class ExceptionHandler(commands.Cog):
-    """Ignore this. This cog handles errors"""
+    """Ignore. This cog handles runtime errors"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        tbc = traceback.format_exception(etype=type(error), value=error, tb=error.__traceback__)
-        tbctext = ''
+        if isinstance(error, commands.CommandNotFound):
+            pass
+        else:
+            tbc = traceback.format_exception(etype=type(error), value=error, tb=error.__traceback__)
+            tbctext = ''
 
-        for i in tbc:
-            tbctext += i
-        
-        url = os.environ["WEBHOOK_URL"]
-        timestamp = str(datetime.datetime.utcnow())
-        await ctx.send('An internal error occurred! We are very sorry.\nPease DM the following to `Zukashi#7071`:')
-        await ctx.send('`DPY-001_{}`'.format(timestamp))
-        msg = 'DPY-001_{}\n```{}```'.format(timestamp, tbctext)
-        data = {
-            'content': msg
-        }
+            for i in tbc:
+                tbctext += i
 
-        requests.post(url, data)
+            url = os.environ['ERROR_WEBHOOK']
+            timestamp = str(datetime.datetime.utcnow())
+            await ctx.send('An internal error occurred! We are very sorry.\nPease DM the following to `Zukashi#7071`:')
+            await ctx.send('`DPY-001_{}`'.format(timestamp))
+            msg = 'DPY-001_{}\n```{}```'.format(timestamp, tbctext)
+            data = {
+                'content': msg
+            }
+
+            requests.post(url, data)
+
+    # @commands.command()
+    # async def errortest(self, ctx):
+    #     raise Exception('valid exception')
 
 def setup(bot):
     bot.add_cog(ExceptionHandler(bot))
